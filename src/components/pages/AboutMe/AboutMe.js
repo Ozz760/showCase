537b6db94ff42e89422c6c86c69f158700844338
Oscar Silva <oscarsilva2356@gmail.com>
2022-07-09 17:48:51 -0700

    Updated about me

--- src/components/pages/AboutMe/AboutMe.js
+++ src/components/pages/AboutMe/AboutMe.js
@@ -5,10 +5,32 @@ function AboutMe() {
     <div>
       <h1>About Me</h1>
       <p>
-        Hello, my name is Oscar Silva, and I am a full stack web developer. 
+        Hello, my name is Oscar Silva, and I am a Full Stack Developer and I
+        enjoy figuring out how to make something work. My journey for web
+        development started back in December of 2021, when I first took my
+        coding class, I've been hooked ever since.
       </p>
+      <p>
+        Today I am sharpening my skills as a Full Stack Developer, and working
+        on some awesome projects that I proud of. On the side I am teaching
+        myself Java and frameworks like SpringBoot. 
+      </p>
+      <p>
+        Here are some technologies I am currently working with:
+      </p>
+      <ul>
+        <li>JavaScript</li>
+        <li>HTML5</li>
+        <li>CSS3</li>
+        <li>React</li>
+        <li>NodeJS</li>
+        <li>Express</li>
+        <li>MongoDB</li>
+        <li>SpringBoot</li>
+
+      </ul>
     </div>
   );
 }
 
-export default AboutMe; 
+export default AboutMe;
