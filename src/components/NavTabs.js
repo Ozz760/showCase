3bc51cec82402f3ff593d69fd65efb0402b11ccc
Oscar Silva <oscarsilva2356@gmail.com>
2022-07-09 17:04:29 -0700

    Changed the layout of the nav bar

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,24 +1,28 @@
 import React from "react";
 import "./NavTabs.css";
 
-
-
-function NavTabs () {
-    return (
-        <div className="nav">
-        <ul>
-            <li>
-            <a href="#">About</a>
-            </li>
-            <li>
-            <a href="#">Projects</a>
-            </li>
-            <li>
-            <a href="#">Contact</a>
-            </li>
-        </ul>
-        </div>
-    );
+function NavTabs() {
+  return (
+    <div className="nav">
+      <ul class="nav justify-content-end">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">
+            About Me
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            Projects
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            Contact
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-export default NavTabs; 
+export default NavTabs;
