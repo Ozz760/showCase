1652e4c5c0106cfd2cf112845944c9836c1cc16e
Oscar Silva <oscarsilva2356@gmail.com>
2022-07-11 13:21:39 -0700

    Added background image to the header

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -3,11 +3,13 @@ import AboutMe from "./pages/AboutMe/AboutMe";
 import NavTabs from "./NavTabs";
 import Projects from "./pages/Projects/Projects";
 import Contact from "./pages/Contact/Contact";
+import Header from "./pages/Header/Header";
 
 function PortfolioContainer() {
     return (
       <div>
         <NavTabs />
+        <Header />
         <AboutMe />
         <Projects />
         <Contact />
---
+++ src/components/pages/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header-wrapper">
+      <h1 className="title">
+        Software Developer
+      </h1>
+    </div>
+  );
+}
+
+export default Header;
