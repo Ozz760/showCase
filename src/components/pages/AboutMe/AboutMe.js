b74d286fcf42885e4f453fc3d2745de209e3cbd1
Oscar Silva <oscarsilva2356@gmail.com>
2022-07-12 10:47:23 -0700

    made changes to about me and adde profolio picture

--- src/components/pages/AboutMe/AboutMe.js
+++ src/components/pages/AboutMe/AboutMe.js
@@ -1,24 +1,27 @@
 import React from "react";
+import "./AboutMe.css";
+const profile = require("../../images/IMG_1510.jpg");  
 
 function AboutMe() {
   return (
     <div>
-      <h1>About Me</h1>
-      <p>
+      <h1 className="aboutme-title">About Me</h1>
+      <p className="first-par">
         Hello, my name is Oscar Silva, and I am a Full Stack Developer and I
         enjoy figuring out how to make something work. My journey for web
         development started back in December of 2021, when I first took my
         coding class, I've been hooked ever since.
       </p>
-      <p>
+      <p className="second-par">
         Today I am sharpening my skills as a Full Stack Developer, and working
         on some awesome projects that I proud of. On the side I am teaching
         myself Java and frameworks like SpringBoot. 
       </p>
-      <p>
+      <img src={profile} alt="Picture of Oscar Silva" className="img0" />
+      <p className="list-par">
         Here are some technologies I am currently working with:
       </p>
-      <ul>
+      <ul className="list-skills">
         <li>JavaScript</li>
         <li>HTML5</li>
         <li>CSS3</li>
@@ -27,7 +30,6 @@ function AboutMe() {
         <li>Express</li>
         <li>MongoDB</li>
         <li>SpringBoot</li>
-
       </ul>
     </div>
   );
