3dacd7f34f47696f628534d947cd9322194f7744
Oscar Silva <oscarsilva2356@gmail.com>
2022-07-13 13:32:29 -0700

    Got the icons for the project cards

--- src/components/pages/Projects/Projects.js
+++ src/components/pages/Projects/Projects.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Projects.css";
 import Card from "react-bootstrap/Card";
 import { FaGithub } from "react-icons/fa";
+import { FaJs } from "react-icons/fa";
 const RR = require("../../images/RR-2022.png");
 const TE = require("../../images/TE-2022.png");
 const NT = require("../../images/NT-2022.png");
@@ -9,7 +10,7 @@ const NT = require("../../images/NT-2022.png");
 function Projects() {
   const projects = [
     {
-      image: `${RR}`,
+      image: RR,
       title: "Recipe-Roulette",
       description:
         "The app will allow you to randomly generate a recipe along with a shopping list tailored to the ingredients needed to make the meal, as well as display an applicable YouTube video.",
@@ -17,7 +18,7 @@ function Projects() {
       link: "https://kreativekntrl.github.io/project-01/",
     },
     {
-      image: `${TE}`,
+      image: TE,
       title: "Text-Editor-IO",
       description:
         "This is an application that allows users to code to their browsers and have their code automatically save and reloaded when the user leaves the app.",
@@ -25,10 +26,10 @@ function Projects() {
       link: "https://murmuring-depths-08823.herokuapp.com/",
     },
     {
-      image: `${NT}`,
+      image: NT,
       title: "Note-Taker",
       description:
-        "This application allows the user to input notes in a givin prompt.",
+        "This application allows the user to input notes in a given prompt.",
       github: "https://github.com/Ozz760/Note-Taker-Express.js",
       link: "https://lit-cliffs-22896.herokuapp.com/",
     },
@@ -36,12 +37,20 @@ function Projects() {
   const renderProjects = (projects, index) => {
     return (
       <div>
-        <Card style={{ width: "18rem" }} className="box">
-          <Card.Img variant="top" src="holder.js/100px180" />
+        <Card style={{ width: "25rem" }} className="box">
+          <Card.Img variant="top" src={projects.image} />
           <Card.Body>
             <Card.Title>{projects.title}</Card.Title>
             <Card.Text>{projects.description}</Card.Text>
           </Card.Body>
+          <div className="icons">
+            <a href={projects.github} target="_blank" rel="noreferrer">
+              <FaGithub />
+            </a>
+            <a href={projects.link} target="_blank" rel="noreferrer">
+              <FaJs />
+            </a>
+          </div>
         </Card>
       </div>
     );
