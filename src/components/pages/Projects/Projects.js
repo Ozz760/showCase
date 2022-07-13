56e16da72dbdde914e9aa95641f68e1e1f23c39f
Oscar Silva <oscarsilva2356@gmail.com>
2022-07-13 12:35:58 -0700

    Got the card to render the text and added images to the text card

--- src/components/pages/Projects/Projects.js
+++ src/components/pages/Projects/Projects.js
@@ -1,23 +1,56 @@
 import React from "react";
 import "./Projects.css";
 import Card from "react-bootstrap/Card";
-import { Button } from "react-bootstrap";
-import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa"; 
+import { FaGithub } from "react-icons/fa";
+const RR = require("../../images/RR-2022.png");
+const TE = require("../../images/TE-2022.png");
+const NT = require("../../images/NT-2022.png");
 
 function Projects() {
+  const projects = [
+    {
+      image: `${RR}`,
+      title: "Recipe-Roulette",
+      description:
+        "The app will allow you to randomly generate a recipe along with a shopping list tailored to the ingredients needed to make the meal, as well as display an applicable YouTube video.",
+      github: "https://github.com/Ozz760/Recipe-Roulette",
+      link: "https://kreativekntrl.github.io/project-01/",
+    },
+    {
+      image: `${TE}`,
+      title: "Text-Editor-IO",
+      description:
+        "This is an application that allows users to code to their browsers and have their code automatically save and reloaded when the user leaves the app.",
+      github: "https://github.com/Ozz760/Text-Editor-io",
+      link: "https://murmuring-depths-08823.herokuapp.com/",
+    },
+    {
+      image: `${NT}`,
+      title: "Note-Taker",
+      description:
+        "This application allows the user to input notes in a givin prompt.",
+      github: "https://github.com/Ozz760/Note-Taker-Express.js",
+      link: "https://lit-cliffs-22896.herokuapp.com/",
+    },
+  ];
+  const renderProjects = (projects, index) => {
+    return (
+      <div>
+        <Card style={{ width: "18rem" }} className="box">
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>{projects.title}</Card.Title>
+            <Card.Text>{projects.description}</Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+    );
+  };
+
   return (
     <div>
-      <Card style={{ width: "18rem" }} className="box">
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
+      <h1>Projects</h1>
+      {projects.map(renderProjects)}
     </div>
   );
 }
